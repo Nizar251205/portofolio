895fb2c034d53ff3bfaf770e1baf0e37f64c7c0b
Nizar251205 <zulhaqynizar@gmail.com>
2026-09-15 17:02:20 +0700

    Fix storage and runtime config for vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
-// Mengarahkan Vercel ke entry point Laravel
+// Menesuaikan direktori penyimpanan sementara Vercel ke /tmp
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+// Mengarahkan ke public/index.php bawaan Laravel
 require __DIR__ . '/../public/index.php';
