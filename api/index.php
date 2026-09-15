1ab76dc1e11f8ed2e35beeee325698650951d1bf
Nizar251205 <zulhaqynizar@gmail.com>
2026-09-15 17:17:09 +0700

    Fix empty html render

--- api/index.php
+++ api/index.php
@@ -1,11 +1,12 @@
 <?php
 
-// Set folder storage/cache sementara ke /tmp Vercel
+// Menyesuaikan folder penyimpanan sementara untuk serverless Vercel
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 
-// Panggil file public/index.php Laravel
+// Memastikan output HTML tercetak dengan benar
+ob_start();
 require __DIR__ . '/../public/index.php';
