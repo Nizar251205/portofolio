728db0aea4c89f7ac21973b89bb5e809d035385f
Nizar251205 <zulhaqynizar@gmail.com>
2026-09-15 17:13:14 +0700

    Fix blank page routing

--- api/index.php
+++ api/index.php
@@ -1,11 +1,11 @@
 <?php
 
-// Menesuaikan direktori penyimpanan sementara Vercel ke /tmp
+// Set folder storage/cache sementara ke /tmp Vercel
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 
-// Mengarahkan ke public/index.php bawaan Laravel
+// Panggil file public/index.php Laravel
 require __DIR__ . '/../public/index.php';
